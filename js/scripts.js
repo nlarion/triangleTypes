c146c90df569298400f73cea9c97bb2d56dac14a
Neil Larion <neil.larion@gmail.com>
2016-01-19 21:35:36 -0800

    Refactor code to have isTriangle and isValidInput functions and tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,46 @@
 var triangleType = function(triangle){
 
-  var side1 = parseInt(triangle[0]);
-  var side2 = parseInt(triangle[1]);
-  var side3 = parseInt(triangle[2]);
-
-  if((triangle[0].length === 0) || (triangle[1].length === 0) || (triangle[2].length === 0) || isNaN(triangle[0]) || isNaN(triangle[1]) || isNaN(triangle[2]) ) {
-    return "";
-  } else if ((side1 !== side2) && (side3 !== side2) && (side1 !== side3)){
-    if((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)){
-      //not a triangle return blank string
-      return "";
-    }else {
+  if((isValidInput(triangle)) && (isTriangle(triangle))){
+    if((parseInt(triangle[0]) !== parseInt(triangle[1])) && (parseInt(triangle[2]) !== parseInt(triangle[1])) && (parseInt(triangle[0]) !== parseInt(triangle[2]))){
       return "scalene";
+    } else if ((parseInt(triangle[0]) === parseInt(triangle[1])) && (parseInt(triangle[2]) === parseInt(triangle[1]))){
+      return "equilateral";
+    } else if ((parseInt(triangle[0]) === parseInt(triangle[1])) || (parseInt(triangle[2]) === parseInt(triangle[1])) || (parseInt(triangle[0]) === parseInt(triangle[2]))) {
+      return "isosceles";
+    } else {
+      return false;
     }
-  } else if ((side1 === side2) && (side3 === side2)){
-    return "equilateral";
-  } else if ((side1 === side2) || (side3 === side2) || (side1 === side3)) {
-    return "isosceles";
-  } else {
+  } else{
     return false;
   }
-};
+}
+
+function isTriangle(triangle){
+  if ((parseInt(triangle[0]) + parseInt(triangle[1]) <= parseInt(triangle[2])) || (parseInt(triangle[1]) + parseInt(triangle[2]) <= parseInt(triangle[0])) || (parseInt(triangle[0]) + parseInt(triangle[2]) <= parseInt(triangle[1]))){
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function isValidInput(triangle){
+  if((triangle[0].length === 0) || (triangle[1].length === 0) || (triangle[2].length === 0) || isNaN(triangle[0]) || isNaN(triangle[1]) || isNaN(triangle[2]) ){
+    return false;
+  } else{
+    return true;
+  }
+}
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     var triangle =[$("input#side1").val(), $("input#side2").val(), $("input#side3").val() ]
     var result = triangleType(triangle);
-
-    $(".triangle").text(result);
-
-    if (!result) {                 // same as writing if (result === false)
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
+    if(result){
+      $(".triangle").text(result);
+      $("#result").show();
     }
-
-    $("#result").show();
+    
     event.preventDefault();
   });
 });
+
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,44 @@
 describe("triangleType", function(){
 
   it("is not a triangle if the sum of 2 sides is less than or equal to the length of the third side", function(){
-    expect(triangleType([1, 2, 4])).to.equal("");
+    expect(triangleType([1, 2, 4])).to.equal(false);
   });
+
   it("is equilateral if all sides are equal", function(){
     expect(triangleType([2, 2, 2])).to.equal("equilateral");
   });
+
   it("is isosceles if two sides are equal", function(){
     expect(triangleType([2, 2, 3])).to.equal("isosceles");
   });
+
   it("is scalene if no sides are equal", function(){
     expect(triangleType([13, 9, 14])).to.equal("scalene");
   });
+
   it("is false if any side is blank", function(){
-    expect(triangleType(["", 9, 14])).to.equal("");
+    expect(triangleType(["", 9, 14])).to.equal(false);
   });
+
   it("is false if any side is not a integer", function(){
-    expect(triangleType(["hh", 9, 14])).to.equal("");
+    expect(triangleType(["hh", 9, 14])).to.equal(false);
+  });
+
+});
+
+
+describe("isTriangle", function(){
+  it("is true if 3 inputs equals a triangle.", function(){
+    expect(isTriangle([14, 13, 9])).to.equal(true);
+  });
+});
+
+describe("isValidInput", function(){
+  it("is false if at least 1 input is not a number", function(){
+    expect(isValidInput([13, 9, "g"])).to.equal(false);
+  });
+
+  it("is false if at least 1 input is blank", function(){
+    expect(isValidInput([13, 9, ""])).to.equal(false);
   });
 });
